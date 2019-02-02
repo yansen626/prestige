f5bfe43d7a7c56b4c4a847af4a44c50f27f2d024
YANSEN <yansen626@gmail.com>
2019-02-02 17:39:56 +0700

    update home instagram

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -141,7 +141,7 @@
     <!-- #Info end -->
 
     <!-- SnapWidget -->
-    {{--<script src="https://snapwidget.com/js/snapwidget.js"></script>--}}
-    {{--<iframe src="https://snapwidget.com/embed/653851" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%; "></iframe>--}}
-    {{--<div style="font:10px/14px 'Roboto','Helvetica Neue',Arial,Helvetica,sans-serif;font-weight:400;width:100%;text-align:right"><a href="https://snapwidget.com" style="color:#777;text-decoration:none;"></a></div>--}}
+    <script src="https://snapwidget.com/js/snapwidget.js"></script>
+    <iframe src="https://snapwidget.com/embed/654784" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%; "></iframe>
+    {{--<div style="font:10px/14px 'Roboto','Helvetica Neue',Arial,Helvetica,sans-serif;font-weight:400;width:660px;text-align:right"><a href="https://snapwidget.com" style="color:#777;text-decoration:none;">SnapWidget · Instagram Widget</a></div>--}}
 @endsection
