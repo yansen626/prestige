013560c5d68f8d0f50f73c1730b1a10c8f9b399b
YANSEN <yansen626@gmail.com>
2019-03-25 13:55:52 +0700

    update 25 march - coming soon page

---
+++ resources/views/frontend/coming-soon.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.frontend-coming-soon')
+@section('pageTitle', 'NAMA | Personalized Leather Goods')
+@section('content')
+
+    <!-- Soon
+============================================= -->
+    <section class="fullscreen pt-0 pb-0">
+        <div class="bg-section" >
+            <img src="{{asset('/images/Links/teaser.jpg')}}" alt="Background"/>
+        </div>
+        {{--<div class="pos-vertical-center">--}}
+            {{--<div class="container">--}}
+                {{--<div class="row clearfix">--}}
+                    {{--<div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">--}}
+                        {{--<div class="text--center">--}}
+                            {{--<h3 class="text-white">Coming Soon</h3>--}}
+                            {{--<div class="divider--line divider--center divider--medium divider--white mb-30"></div>--}}
+                            {{--<p class="text-white">League impresses you with fully responsiveness and highly customization. We did it in combination of very clean and flexible design.</p>--}}
+                        {{--</div><!-- .heading-10 end -->--}}
+                    {{--</div><!-- .col-md-8 end -->--}}
+                {{--</div>--}}
+                {{--<div class="row">--}}
+                    {{--<div class="col-xs-12 col-sm-12  col-md-12">--}}
+                        {{--<div class="countdown countdown-boxed" data-count-date="2018, 5, 28"></div>--}}
+                    {{--</div><!-- .col-md-12 end -->--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    </section><!-- .page-soon -->
+
+@endsection
